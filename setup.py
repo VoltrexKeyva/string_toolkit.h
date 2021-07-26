19e0cf72290ef66b8389ad88477a9ac93a14dfa1
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-07-26 16:04:48 +0700

    experiment attempt 2

--- setup.py
+++ setup.py
@@ -8,9 +8,8 @@ setup(
         Extension('st', ['python_binding.c'])
     ],
     license='MIT',
-    long_description=open('README_PYTHON.md', 'r', encoding='utf-8').read(),
+    long_description=open('README.txt', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/vierofernando/string_toolkit.h',
-    keywords=['string', 'string-manipulation', 'c', 'cpp', 'c-plus-plus'],
-    install_requires=[]
+    keywords=['string', 'string-manipulation', 'c', 'cpp', 'c-plus-plus']
 )
