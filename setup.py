0e18fa0b0ae7026969e27dd2114deca6b472f371
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-07-26 15:44:07 +0700

    nice again

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from distutils.core import setup, Extension
 
 setup(
-    name="string_toolkit",
-    version="1.0.0",
-    description="Python wrapper for the string_toolkit.h C/C++ header",
+    name='string_toolkit',
+    version='0.0.1',
+    description='Python wrapper for the string_toolkit.h C/C++ header',
     ext_modules=[
-        Extension("st", ["python_binding.c"])
-    ]
+        Extension('st', ['python_binding.c'])
+    ],
+    license='MIT',
+    description='A free and asynchronous Weather API Wrapper.',
+    long_description=open('README_PYTHON.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vierofernando/string_toolkit.h',
+    download_url='https://github.com/vierofernando/string_toolkit.h/archive/0.0.1.tar.gz',
+    keywords=['Weather', 'API', 'Weather API', 'API Wrapper', 'Weather CLI', 'CLI'],
+    install_requires=[]
 )
