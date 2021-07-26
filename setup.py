060fd85c73e3e8b80183c1f98c4056f4bb8643a3
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-07-26 16:02:35 +0700

    experiment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name='string-toolkit',
-    version='0.0.1',
+    version='0.0.2',
     description='Python wrapper for the string_toolkit.h C/C++ header',
     ext_modules=[
         Extension('st', ['python_binding.c'])
@@ -11,7 +11,6 @@ setup(
     long_description=open('README_PYTHON.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/vierofernando/string_toolkit.h',
-    download_url='https://github.com/vierofernando/string_toolkit.h/archive/0.0.1.tar.gz',
     keywords=['string', 'string-manipulation', 'c', 'cpp', 'c-plus-plus'],
     install_requires=[]
 )
