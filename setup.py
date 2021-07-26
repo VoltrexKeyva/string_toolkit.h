d62bd890c198dda71f82e6300f08e1d6d41ea139
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-07-26 16:12:29 +0700

    nice

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 setup(
     name='string-toolkit',
-    version='0.0.2',
+    version='0.0.3',
     description='Python wrapper for the string_toolkit.h C/C++ header',
     ext_modules=[
         Extension('st', ['python_binding.c'])
     ],
     license='MIT',
-    long_description=open('README.txt', 'r', encoding='utf-8').read(),
+    long_description=open('README_PYTHON.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/vierofernando/string_toolkit.h',
     keywords=['string', 'string-manipulation', 'c', 'cpp', 'c-plus-plus']
